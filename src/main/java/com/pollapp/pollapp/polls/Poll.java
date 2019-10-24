0ae2ab322f601e4279e4b2461654f045878ede62
Nguyen <tram.nguyen@materna.de>
2019-10-24 12:52:02 +0200

    added customized response

--- src/main/java/com/pollapp/pollapp/polls/Poll.java
+++ src/main/java/com/pollapp/pollapp/polls/Poll.java
@@ -1,15 +1,19 @@
 package com.pollapp.pollapp.polls;
 
+import com.pollapp.pollapp.choices.Choice;
 import com.pollapp.pollapp.dateAudit.UserDateAudit;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Poll extends UserDateAudit {
     @Id
@@ -20,5 +24,46 @@ public class Poll extends UserDateAudit {
     private String question;
     @NotNull
     private Instant expirationDateTime;
+    @OneToMany(
+            mappedBy = "poll",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER,
+            orphanRemoval = true
+    )
+    @Size(min = 2, max = 6)
+    @Fetch(FetchMode.SELECT)
+    @BatchSize(size = 30)
+    private List<Choice> choices = new ArrayList<>();
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Instant getExpirationDateTime() {
+        return expirationDateTime;
+    }
+
+    public void setExpirationDateTime(Instant expirationDateTime) {
+        this.expirationDateTime = expirationDateTime;
+    }
+
+    public List<Choice> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
 }
